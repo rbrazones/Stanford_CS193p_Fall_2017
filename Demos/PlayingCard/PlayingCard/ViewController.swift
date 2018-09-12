b4770e8afbbb363cce67908aae9f8fcfd221e740
Ryan Brazones <rbrazones@gmail.com>
2018-09-11 20:50:58 -0700

    Finished Lecture 6

--- Demos/PlayingCard/PlayingCard/ViewController.swift
+++ Demos/PlayingCard/PlayingCard/ViewController.swift
@@ -12,6 +12,32 @@ class ViewController: UIViewController {
 
     var deck = PlayingCardDeck()
     
+    @IBOutlet weak var PlayingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left,.right]
+            PlayingCardView.addGestureRecognizer(swipe)
+            
+            let pinch = UIPinchGestureRecognizer(target: PlayingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
+            PlayingCardView.addGestureRecognizer(pinch)
+        }
+    }
+    
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            PlayingCardView.rank = card.rank.order
+            PlayingCardView.suit = card.suit.rawValue
+        }
+    }
+    
+    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+            PlayingCardView.isFaceUp = !PlayingCardView.isFaceUp
+        default: break
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
