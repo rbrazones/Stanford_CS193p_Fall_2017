c1bb19b2e5799fa35fbcbbfe3a70c6de53232760
Ryan Brazones <rbrazones@gmail.com>
2018-09-13 23:56:43 -0700

    A lot of work to do, not sure where to start

---
+++ Assignment_3_Graphical_Set/setGame/setGame/SetCardView.swift
@@ -0,0 +1,20 @@
+//
+//  SetCardView.swift
+//  setGame
+//
+//  Created by Ryan Brazones on 9/13/18.
+//  Copyright © 2018 greenred. All rights reserved.
+//
+
+import UIKit
+
+class SetCardView: UIView {
+    
+    @IBInspectable
+    var color: UIColor = UIColor.red
+    
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+
+}
