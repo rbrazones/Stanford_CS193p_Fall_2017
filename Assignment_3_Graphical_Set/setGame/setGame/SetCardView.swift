4c9609808a4710f96baaeea5f41fa8fd905a4d88
Ryan Brazones <rbrazones@gmail.com>
2018-09-15 23:54:20 -0700

    Drawing stripes now works. I think IBInspectable win the set card view will crash xcode now for some reason

--- Assignment_3_Graphical_Set/setGame/setGame/SetCardView.swift
+++ Assignment_3_Graphical_Set/setGame/setGame/SetCardView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-@IBDesignable
+//@IBDesignable
 class SetCardView: UIView {
     
     enum withShape: String {
@@ -26,7 +26,7 @@ class SetCardView: UIView {
     }
     
     @IBInspectable
-    var color: UIColor = UIColor.red { didSet { setNeedsDisplay() } }
+    var color: UIColor = UIColor.purple { didSet { setNeedsDisplay() } }
     
     @IBInspectable
     var number: Int = 1 { didSet { setNeedsDisplay() } }
@@ -34,65 +34,80 @@ class SetCardView: UIView {
     @IBInspectable
     var isSelected: Bool = false { didSet {setNeedsDisplay()}}
     
+    @IBInspectable
+    var cardBackgroundColor: UIColor = UIColor.white { didSet{ setNeedsDisplay() }}
+    
+    @IBInspectable
+    var cardSelectOutlineColor: UIColor = UIColor.red { didSet{ setNeedsDisplay() }}
+    
     var shape: withShape = .diamond
-    var shade: withShade = .unfilled
+    var shade: withShade = .striped
     
     override func draw(_ rect: CGRect) {
         
+        // There will be situations where we want to draw more than 1 shape on a given
+        // card. We are using addClip() to simplify the process of drawing the stripes
+        // inside the shapes. Thus, it is necessary to save/restore the graphics context
+        // so the addClip() calls from one shape do not interfere with the drawing of
+        // the other shapes on the card.
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
         drawCardBackground()
-        print("draw()")
+        
+        context.saveGState()
         
         let center = CGPoint(x: bounds.midX, y: bounds.midY)
         drawDiamond(at: center)
-        drawTestRect(at: center)
+        //drawTestRect(at: center)
+        
+        context.restoreGState()
+        context.saveGState()
         
         let upperPoint = center.offsetBy(dx: 0, dy: -shapeHeight - (shapeHeight / 4.0))
         drawOval(at: upperPoint)
-        drawTestRect(at: upperPoint)
+        //drawTestRect(at: upperPoint)
+        
+        context.restoreGState()
+        context.saveGState()
         
         let lowerPoint = center.offsetBy(dx: 0, dy: shapeHeight + (shapeHeight / 4.0))
         drawSquiggle(at: lowerPoint)
-        drawTestRect(at: lowerPoint)
+        //drawTestRect(at: lowerPoint)
+        
+        context.restoreGState()
     }
 
 }
 
 extension SetCardView {
-    
+
     private func drawTestRect(at point: CGPoint) {
         let test = UIBezierPath()
-        
         let distanceToCenter = (shapeWidth / 2 - shapeHeight / 2)
-        
         test.move(to: point.offsetBy(dx: -shapeWidth / 2, dy: shapeHeight / 2))
         test.addLine(to: test.currentPoint.offsetBy(dx: 0, dy: -shapeHeight))
         test.addLine(to: test.currentPoint.offsetBy(dx: shapeWidth, dy: 0))
         test.addLine(to: test.currentPoint.offsetBy(dx: 0, dy: shapeHeight))
         test.addLine(to: test.currentPoint.offsetBy(dx: -shapeWidth, dy: 0))
-        
         test.move(to: point.offsetBy(dx: -shapeWidth / 2, dy: 0))
         test.addLine(to: test.currentPoint.offsetBy(dx: shapeWidth, dy: 0))
-        
         test.move(to: point.offsetBy(dx: -distanceToCenter, dy: shapeHeight / 2))
         test.addLine(to: test.currentPoint.offsetBy(dx: 0, dy: -shapeHeight))
-        
         test.move(to: point.offsetBy(dx: distanceToCenter, dy: shapeHeight / 2))
         test.addLine(to: test.currentPoint.offsetBy(dx: 0, dy: -shapeHeight))
-        
         UIColor.black.setStroke()
         test.stroke()
     }
     
     private func drawCardBackground() {
-        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: 16.0)
+        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: constantValues.cardCornerRadius)
         roundedRect.addClip()
-        UIColor.white.setFill()
-        UIColor.red.setStroke()
-        roundedRect.lineWidth = 7.5
+        cardBackgroundColor.setFill()
+        cardSelectOutlineColor.setStroke()
+        roundedRect.lineWidth = constantValues.cardSelectLineWidth
         roundedRect.fill()
-        roundedRect.stroke()
-        
-        print("Draw the background")
+        if (isSelected) { roundedRect.stroke() }
     }
     
     private func drawDiamond(at point: CGPoint) {
@@ -102,10 +117,14 @@ extension SetCardView {
         diamondPath.addLine(to: point.offsetBy(dx: shapeWidth / 2, dy: 0))
         diamondPath.addLine(to: point.offsetBy(dx: 0, dy: shapeHeight / 2))
         diamondPath.addLine(to: point.offsetBy(dx: -shapeWidth / 2, dy: 0))
-        UIColor.green.setStroke()
-        UIColor.green.setFill()
+        color.setStroke()
+        color.setFill()
+        diamondPath.lineWidth = constantValues.shapeLineWidth
+        diamondPath.addClip()
         diamondPath.stroke()
-        diamondPath.fill()
+        if (shade == .unfilled) { return }
+        else if (shade == .striped) { drawStripesInShape(at: point) }
+        else if (shade == .solid) { diamondPath.fill() }
     }
     
     private func drawOval(at point: CGPoint) {
@@ -117,10 +136,14 @@ extension SetCardView {
         ovalPath.addLine(to: point.offsetBy(dx: distanceToCenter, dy: -shapeHeight / 2))
         ovalPath.addArc(withCenter: rightCenter, radius: shapeHeight / 2, startAngle: 3 * CGFloat.pi / 2, endAngle: CGFloat.pi / 2, clockwise: true)
         ovalPath.addLine(to: point.offsetBy(dx: -distanceToCenter, dy: shapeHeight / 2))
-        UIColor.red.setFill()
-        UIColor.red.setStroke()
+        color.setStroke()
+        color.setFill()
+        ovalPath.lineWidth = constantValues.shapeLineWidth
+        ovalPath.addClip()
         ovalPath.stroke()
-        ovalPath.fill()
+        if (shade == .unfilled) { return }
+        else if (shade == .striped) { drawStripesInShape(at: point) }
+        else if (shade == .solid) { ovalPath.fill() }
     }
     
     private func drawSquiggle(at point: CGPoint) {
@@ -128,68 +151,91 @@ extension SetCardView {
         let distanceToCenter = (shapeWidth / 2 - shapeHeight / 2)
         let upperLeftCenter = point.offsetBy(dx: -distanceToCenter, dy: 0)
         let lowerRightCenter = point.offsetBy(dx: distanceToCenter, dy: 0)
-        let testX = shapeWidth / 8
-        let testY = shapeHeight / 4
+        let unitX = shapeWidth / 8  // breaking down shape width into discrete units
+        let unitY = shapeHeight / 4 // breaking down shape height into discrete units
         
         // upper left
         squigglePath.move(to: point.offsetBy(dx: -shapeWidth / 2, dy: 0))
         squigglePath.addArc(withCenter: upperLeftCenter, radius: shapeHeight / 2, startAngle: CGFloat.pi, endAngle: 3 * CGFloat.pi / 2, clockwise: true)
         
         // top middle
-        var cp1 = point.offsetBy(dx: 0, dy: -shapeHeight / 2)
-        var cp2 = point.offsetBy(dx: 0, dy: testY)
-        var endPoint = point.offsetBy(dx: 2 * testX, dy: -testY)
-        squigglePath.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
+        var curveControlPoint1 = point.offsetBy(dx: 0, dy: -shapeHeight / 2)
+        var curveControlPoint2 = point.offsetBy(dx: 0, dy: unitY)
+        var endPoint = point.offsetBy(dx: 2 * unitX, dy: -unitY)
+        squigglePath.addCurve(to: endPoint, controlPoint1: curveControlPoint1, controlPoint2: curveControlPoint2)
         
-        cp1 = point.offsetBy(dx: 2.5 * testX, dy: -1.5 * testY)
-        cp2 = point.offsetBy(dx: 2.5 * testX, dy: -2 * testY)
-        endPoint = point.offsetBy(dx: 3 * testX, dy: -2 * testY)
-        squigglePath.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
+        curveControlPoint1 = point.offsetBy(dx: 2.5 * unitX, dy: -1.5 * unitY)
+        curveControlPoint2 = point.offsetBy(dx: 2.5 * unitX, dy: -2 * unitY)
+        endPoint = point.offsetBy(dx: 3 * unitX, dy: -2 * unitY)
+        squigglePath.addCurve(to: endPoint, controlPoint1: curveControlPoint1, controlPoint2: curveControlPoint2)
         
         // upper right
-        cp1 = point.offsetBy(dx: testX * 3.5, dy: -testY * 2)
-        cp2 = point.offsetBy(dx: testX * 4, dy: -testY)
+        curveControlPoint1 = point.offsetBy(dx: 3.5 * unitX, dy: -2 * unitY)
+        curveControlPoint2 = point.offsetBy(dx: 4 * unitX, dy: -unitY)
         endPoint = point.offsetBy(dx: shapeWidth / 2, dy: 0)
-        squigglePath.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
+        squigglePath.addCurve(to: endPoint, controlPoint1: curveControlPoint1, controlPoint2: curveControlPoint2)
         
         // lower right (symmetric to upper left)
         squigglePath.addArc(withCenter: lowerRightCenter, radius: shapeHeight / 2, startAngle: 0, endAngle: CGFloat.pi / 2, clockwise: true)
         
         // lower middle
-        cp1 = point.offsetBy(dx: 0, dy: shapeHeight / 2)
-        cp2 = point.offsetBy(dx: 0, dy: -testY)
-        endPoint = point.offsetBy(dx: -2 * testX, dy: testY)
-        squigglePath.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
+        curveControlPoint1 = point.offsetBy(dx: 0, dy: shapeHeight / 2)
+        curveControlPoint2 = point.offsetBy(dx: 0, dy: -unitY)
+        endPoint = point.offsetBy(dx: -2 * unitX, dy: unitY)
+        squigglePath.addCurve(to: endPoint, controlPoint1: curveControlPoint1, controlPoint2: curveControlPoint2)
         
-        cp1 = point.offsetBy(dx: -2.5 * testX, dy: 1.5 * testY)
-        cp2 = point.offsetBy(dx: -2.5 * testX, dy: 2 * testY)
-        endPoint = point.offsetBy(dx: -3 * testX, dy: 2 * testY)
-        squigglePath.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
+        curveControlPoint1 = point.offsetBy(dx: -2.5 * unitX, dy: 1.5 * unitY)
+        curveControlPoint2 = point.offsetBy(dx: -2.5 * unitX, dy: 2 * unitY)
+        endPoint = point.offsetBy(dx: -3 * unitX, dy: 2 * unitY)
+        squigglePath.addCurve(to: endPoint, controlPoint1: curveControlPoint1, controlPoint2: curveControlPoint2)
         
         // lower left (symmertric to upper right)
-        cp1 = point.offsetBy(dx: -3.5 * testX, dy: 2 * testY)
-        cp2 = point.offsetBy(dx: -4 * testX, dy: testY)
+        curveControlPoint1 = point.offsetBy(dx: -3.5 * unitX, dy: 2 * unitY)
+        curveControlPoint2 = point.offsetBy(dx: -4 * unitX, dy: unitY)
         endPoint = point.offsetBy(dx: -shapeWidth / 2, dy: 0)
-        squigglePath.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
+        squigglePath.addCurve(to: endPoint, controlPoint1: curveControlPoint1, controlPoint2: curveControlPoint2)
         
-        UIColor.purple.setStroke()
-        UIColor.purple.setFill()
+        color.setStroke()
+        color.setFill()
+        squigglePath.lineWidth = constantValues.shapeLineWidth
+        squigglePath.addClip()
         squigglePath.stroke()
-        squigglePath.fill()
-        
+        if (shade == .unfilled) { return }
+        else if (shade == .striped) { drawStripesInShape(at: point) }
+        else if (shade == .solid) { squigglePath.fill() }
+    }
+    
+    private func drawStripesInShape(at point: CGPoint) {
+        let stripePath = UIBezierPath()
+        let startPoint = point.offsetBy(dx: -shapeWidth / 2, dy: shapeHeight / 2)
+        stripePath.move(to: startPoint)
+        let distanceBetweenStripes = shapeWidth / constantValues.numberOfStripesToDraw
+        var currentStripeOffset: CGFloat = 0
+        var stripeCounter: Int = 0
+        while (stripeCounter < Int(constantValues.numberOfStripesToDraw)) {
+            stripePath.addLine(to: stripePath.currentPoint.offsetBy(dx: 0, dy: -shapeHeight))
+            currentStripeOffset += distanceBetweenStripes
+            stripePath.move(to: startPoint.offsetBy(dx: currentStripeOffset, dy: 0))
+            stripeCounter += 1
+        }
+        stripePath.lineWidth = constantValues.stripeLineWidth
+        color.setStroke()
+        stripePath.stroke()
     }
     
     private struct constantValues {
-        
+        static let cardCornerRadius: CGFloat = 16.0
+        static let cardSelectLineWidth: CGFloat = 7.5
+        static let shapeLineWidth: CGFloat = 3.0
+        static let numberOfStripesToDraw: CGFloat = 25.0
+        static let stripeLineWidth: CGFloat = 1.0
     }
     
     private var shapeHeight: CGFloat {
-        //return bounds.size.height / 4.0
         return bounds.size.height / 4.5
     }
     
     private var shapeWidth: CGFloat {
-        //return bounds.size.width - (shapeHeight / 4.0)
         return bounds.size.width / 1.5
     }
 }
